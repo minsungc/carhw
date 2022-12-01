958fe4c8835a83f87573d90ac560d87fc21627f0
Minsung <minsung@ccs.neu.edu>
2022-12-01 11:58:44 -0500

    mostly done

--- project/wasm.lisp
+++ project/wasm.lisp
@@ -98,48 +98,44 @@ to do the either-or we define the type wasm-eval-h-out,
 which is a sum type of either an exception String or a program stack.
 |#
 
+(defdata wasm-ops-h-out (oneof String int))
 (defdata wasm-eval-h-out (oneof String stack))
 
-(definec bit-listp (l :tl) :bool
-       (cond ((atom l) (eq l nil))
-             (t (and (bitp (car l))
-                     (bit-listp (cdr l))))))
+(defdata bitlist (listof (oneof 0 1)))
 
 ;; integer to bits as a tl 
-(definec int-to-bits-helper (x :int b :tl) :tl
+(definec int-to-bits-helper (x :i64 b :bitlist) :bitlist
   (if (> x 1)
-      (int-to-bits-helper (truncate x 2) (append (list (rem x 2)) b))
+      (int-to-bits-helper (truncate x 2) (cons (rem x 2) b))
     (append (list x) b)))
-(definec int-to-bits (x :int) :tl
+(definec int-to-bits (x :i64) :bitlist
   (int-to-bits-helper x nil))
 
 ;; bits to integer 
-(definec bits-to-int-helper (bits :tl ct :nat ret :int) :int
-  :skip-admissibilityp t
-  :skip-body-contractsp t
-  (if (or (not (bit-listp bits)) (> ct 64)) 0
-    (if (or (= ct 63) (not (nth ct bits))) ret
-      (bits-to-int-helper bits (+ ct 1) (+ (* (nth ct bits) (expt 2 ct)) ret)))))
+(definec bits-to-int-helper (bits :bitlist ct :nat ret :int) :int
+  (match bits
+    (nil ret)
+    ((f . r) (bits-to-int-helper r (+ ct 1) (+ ret (* f (expt 2 ct)))))))
 
-(definec bits-to-int (bits :tl) :int
+(property use (bits :bitlist)
+  (bitlistp (rev bits)))
+
+(definec bits-to-int (bits :bitlist) :int
   (bits-to-int-helper (rev bits) 0 0))
 
 
-(definec count-trailing-zeros-helper (bits :tl) :nat
-  :skip-admissibilityp t
-  (if (not (bit-listp bits)) 0 
+(definec count-trailing-zeros-h (bits :bitlist) :nat
   (match bits
     (nil 0)
-    ((f . r) (if (zerop f) (+ 1 (count-trailing-zeros r)) 0)))))
+    ((f . r) (if (zerop f) (+ 1 (count-trailing-zeros-h r)) 0))))
 
-(definec count-trailing-zeros (bits :tl) :nat
-  (count-trailing-zeros-helper (rev bits)))
+(definec count-trailing-zeros (bits :bitlist) :nat
+  (count-trailing-zeros-h (rev bits)))
 
-(definec count-non-zero-bits (bits :tl) :nat
-  (if (or (= 1 (car bits)) (= 0 (car bits)))
-      (+ (car bits) (count-non-zero-bits (cdr bits)))
-    0))
-
+(definec count-non-zero-bits (bits :bitlist) :nat
+  (match bits
+    (nil 0)
+    ((f . r) (+ f (count-non-zero-bits r)))))
 
 (definec wasm-eval-h-i32unop (i :i32unop s :stack) :wasm-eval-h-out
   (let ((x (car s)))
@@ -157,35 +153,40 @@ which is a sum type of either an exception String or a program stack.
 
 
 ;; invert all the bits
-(definec invert-bits (bits :tl) :tl
+(definec invert-bits (bits :bitlist) :bitlist
   (if (consp bits)
-      (if (== 0 (car bits))
+      (if (= 0 (car bits))
 	  (cons 1 (invert-bits (cdr bits)))
 	  (cons 0 (invert-bits (cdr bits))))
       nil))
 
 ;; this takes the reverse of bits as input 
-(definec twos-complement-helper (bits :tl carry :nat) :tl
+(definec twos-complement-helper (bits :bitlist carry :nat) :bitlist
   (if (consp bits)
-      (if (== 0 (car bits)) ;; I should technically write this in match but i don't trust match 
-	  (if (== 0 carry)
+      (if (= 0 (car bits)) ;; I should technically write this in match but i don't trust match 
+	  (if (= 0 carry)
 	      (append (twos-complement-helper (cdr bits) 0) '(0))
 	    (append (twos-complement-helper (cdr bits) 0) '(1)))
-	(if (== 0 carry)
+	(if (= 0 carry)
 	    (append (twos-complement-helper (cdr bits) 0) '(1))
           (append (twos-complement-helper (cdr bits) 1) '(0))))
     nil))
     
 ;; invert all the bits and then add 1
-(definec twos-complement (bits :tl) :tl
+(definec twos-complement (bits :bitlist) :bitlist
   (twos-complement-helper (rev (invert-bits bits)) 1))
-	
 
 ;; if the value is between 0 and 2^31 then return the value
 ;; else return the integer value of the twos complement of i
 (definec sign-interpretation-32 (i :i32) :i32
   :skip-function-contractp t
   (if (and (>= i 0) (< i (expt 2 31)))
+      i
+    (bits-to-int (twos-complement (int-to-bits i)))))
+
+(definec sign-interpretation-64 (i :i64) :i64
+  :skip-function-contractp t
+  (if (and (>= i 0) (< i (expt 2 63)))
       i
       (bits-to-int (twos-complement (int-to-bits i)))))
 
@@ -205,86 +206,162 @@ which is a sum type of either an exception String or a program stack.
 	('extend_u32 (cons x (cdr s)))))))
 
 ;; bitwise and
-(definec bit-and-h (x :tl y :tl ct :nat ret :tl) :int
-  (if (= 0 ct) (bits-to-int ret) (bit-and-h x y (- ct 1) (cons (and (nth ct x) (nth ct y)) ret))))
+(definec bits-and-h (bx :bitlist by :bitlist ret :bitlist) :int
+  (match (list bx by)
+    ((:or (nil &) (& nil)) (bits-to-int ret))
+    (((f . r) (g . s)) (bits-and-h r s (cons (* f g) ret)))))
 
-(definec bits-and (x :i32 y :i32) :i32
-  :skip-function-contractp t
+(definec bits-and (x :i64 y :i64) :int
   (let ((bx (rev (int-to-bits x)))
         (by (rev (int-to-bits y))))
-    (bit-and-h bx by 31 nil)))
+    (bits-and-h bx by nil)))
 
-(definec bit-or-h (x :tl y :tl ct :nat ret :tl) :int
-  (if (= 0 ct) (bits-to-int ret) (bit-and-h x y (- ct 1) (cons (or (nth ct x) (nth ct y)) ret))))
+;; bitwise or
+(definec bits-or-h (bx :bitlist by :bitlist ret :bitlist) :int
+  (match (list bx by)
+    ((nil nil) (bits-to-int ret))
+    ((nil (g . s)) (bits-or-h nil s (cons g ret)))
+    (((f . r) nil) (bits-or-h r nil (cons f ret)))
+    (((f . r) (g . s)) (bits-or-h r s (cons (max f g) ret)))))
 
-(definec bits-or (x :i32 y :i32) :i32
-  :skip-function-contractp t
+(definec bits-or (x :i64 y :i64) :int
   (let ((bx (rev (int-to-bits x)))
         (by (rev (int-to-bits y))))
-   (bit-and-h bx by 31 nil)))
+    (bits-or-h bx by nil)))
 
-(definec bit-xor-h (x :tl y :tl ct :nat ret :tl) :int
-  (if (= 0 ct) (bits-to-int ret) (bit-and-h x y (- ct 1) (cons (xor (nth ct x) (nth ct y)) ret))))
+;; bitwise xor
+(definec bits-xor-h (bx :bitlist by :bitlist ret :bitlist) :int
+  (match (list bx by)
+    ((nil nil) (bits-to-int ret))
+    ((nil (g . s)) (bits-xor-h nil s (cons g ret)))
+    (((f . r) nil) (bits-xor-h r nil (cons f ret)))
+    (((f . r) (g . s)) (bits-xor-h r s (cons (rem (+ f g) 2) ret)))))
 
-(definec bits-xor (x :i32 y :i32) :i32
-  :skip-function-contractp t
+(definec bits-xor (x :i64 y :i64) :int
   (let ((bx (rev (int-to-bits x)))
         (by (rev (int-to-bits y))))
-   (bit-and-h bx by 31 nil)))
+    (bits-xor-h bx by nil)))
+
+(definec wasm-eval-h-i32binop-arith (i :i32binop x :i32 y :i32) :wasm-ops-h-out
+  (match i
+    ('add32 (rem (+ x y) (expt 2 32)))
+    ('sub32 (rem (+ (- x y) (expt 2 32)) (expt 2 32)))
+    ('mul32 (rem (* x y) (expt 2 32)))
+    ('div_u32 (if (zerop y) "div by 0 error" (truncate x y)))
+    ('rem_u32 (if (zerop y) "rem by 0 error" (rem x y)))
+    ('div_s32 (let ((sx (sign-interpretation-32 x)) (sy (sign-interpretation-32 y)))
+		   (if (zerop sy) "div by 0 error"
+		   (if (= (truncate sx sy) (expt 2 31)) "signed div by 0 error" (truncate sx sy)))))
+    ('rem_s32 (let ((sx (sign-interpretation-32 x)) (sy (sign-interpretation-32 y)))
+		(if (zerop sy) "rem by 0 error" (rem sx sy))))
+    (& "this error shouldn't happen. in: wasm-eval-h-i32binop-arith")))
+
+(definec wasm-eval-h-i32binop-bitwise (i :i32binop x :i32 y :i32) :wasm-ops-h-out
+  (match i
+    ('and32 (bits-and x y))
+    ('or32 (bits-or x y))
+    ('xor32 (bits-xor x y))
+    (& "this error shouldn't happen. in: wasm-eval-h-i32binop-bitwise")))
+
+(definec wasm-eval-h-i32binop-compare (i :i32binop x :i32 y :i32) :wasm-ops-h-out
+  (match i
+    ('eq32 (if (= x y) 1 0))
+    ('ne32 (if (= x y) 0 1))
+    ('lt_s32 (if (< (sign-interpretation-32 x) (sign-interpretation-32 y)) 1 0))
+    ('lt_u32 (if (< x y) 1 0))
+    ('gt_u32 (if (> x y) 1 0))
+    ('gt_s32 (if (> (sign-interpretation-32 x) (sign-interpretation-32 y)) 1 0))
+    ('le_u32 (if (<= x y) 1 0))
+    ('le_s32 (if (<= (sign-interpretation-32 x) (sign-interpretation-32 y)) 1 0))
+    ('ge_u32 (if (>= x y) 1 0))
+    ('ge_s32 (if (>= (sign-interpretation-32 x) (sign-interpretation-32 y)) 1 0))
+    (& "this error shouldn't happen. in: wasm-eval-h-i32binop-compare")))
 
 
 (definec wasm-eval-h-i32binop (i :i32binop s :stack) :wasm-eval-h-out
-  (let ((x (car s))
-        (y (cadr s)))
-    (if (or (not x) (not y))
-      "not enough variables declared to do i32binop!"
-      (if (and (i32p x) (i32p y))
-      (match i
-	('add32 (cons (rem (+ x y) (expt 2 32)) (cddr s)))
-	('sub32 (cons (rem (+ (- x y) (expt 2 32)) (expt 2 32)) (cddr s)))
-	('mul32 (cons (rem (* x y) (expt 2 32))(cddr s)))
+  :skip-function-contractp t
+  (match s
+    (nil "not enough arguments!")
+    ((x . k)
+     (match k
+       (nil "not enough arguments!")
+       ((y . r) (if (and (i32p x) (i32p y))
+                    (match i
+	            ((:or 'add32 'sub32 'mul32 'div_u32 'rem_u32 'div_s32 'rem_s32)
+	             (let ((res (wasm-eval-h-i32binop-arith i x y)))
+		       (if (not (constp x)) "result was not a const"
+			 (if (wasm-eval-h-outp res) res (cons res r)))))  
+	            ((:or 'and32 'or32 'xor32)
+	             (let ((res (wasm-eval-h-i32binop-bitwise i x y)))
+		       (if (not (constp x)) "result was not a const"
+			 (if (wasm-eval-h-outp res) res (cons res r)))))  
+                   (& (let ((res (wasm-eval-h-i32binop-compare i x y)))
+			(if (not (constp x)) "result was not a const"
+			  (if (wasm-eval-h-outp res) res (cons res r)))))) 
+	         "not i32 inputs!"))
+       ))))
 
-	('div_u32 (if (zerop y) "div by 0 error" (cons (truncate x y) (cddr s))))
-	('rem_u32 (if (zerop y) "rem by 0 error" (cons (rem x y) (cddr s))))
-	('div_s32 (let ((sx (sign-interpretation-32 x))
-			 (sy (sign-interpretation-32 y)))
-		     (if (zerop sy) "div by 0 error"
-		       (if (= (truncate sx sy) (expt 2 31)) "signed div by 0 error" (cons (truncate sx sy) (cddr s))))))
-	('rem_s32 (let ((sx (sign-interpretation-32 x))
-			 (sy (sign-interpretation-32 y)))
-		     (if (zerop sy) "rem by 0 error" (cons (rem sx sy) (cddr s)))))
-	
-	('and32 (cons (bit-and x y) (cddr s)))
-	('or32 (cons (bit-or x y) (cddr s)))
-	('xor32 (cons (bit-xor x y) (cddr s)))
 
-	('eq32 if )
-	('ne32 "TODO")
-	('lt_s32 "TODO")
-	('lt_u32 "TODO")
-	('gt_u32 "TODO")
-	('gt_s32 "TODO")
-	('le_u32 "TODO")
-	('le_s32 "TODO")
-	('ge_u32 "TODO")
-	('ge_s32 "TODO")	
+;; 64 bit binary ops
+(definec wasm-eval-h-i64binop-arith (i :i64binop x :i64 y :i64) :wasm-ops-h-out
+  (match i
+    ('add32 (rem (+ x y) (expt 2 64)))
+    ('sub32 (rem (+ (- x y) (expt 2 64)) (expt 2 64)))
+    ('mul32 (rem (* x y) (expt 2 64)))
+    ('div_u32 (if (zerop y) "div by 0 error" (truncate x y)))
+    ('rem_u32 (if (zerop y) "rem by 0 error" (rem x y)))
+    ('div_s32 (let ((sx (sign-interpretation-64 x)) (sy (sign-interpretation-64 y)))
+		   (if (zerop sy) "div by 0 error"
+		   (if (= (truncate sx sy) (expt 2 64)) "signed div by 0 error" (truncate sx sy)))))
+    ('rem_s32 (let ((sx (sign-interpretation-64 x)) (sy (sign-interpretation-64 y)))
+		(if (zerop sy) "rem by 0 error" (rem sx sy))))
+    (& "this error shouldn't happen. in: wasm-eval-h-i64binop-arith")))
 
-	) "not i32 inputs!")))
-  :timeout 300)
+(definec wasm-eval-h-i64binop-bitwise (i :i64binop x :i64 y :i64) :wasm-ops-h-out
+  (match i
+    ('and64 (bits-and x y))
+    ('or64 (bits-or x y))
+    ('xor64 (bits-xor x y))
+    (& "this error shouldn't happen. in: wasm-eval-h-i64binop-bitwise")))
+
+(definec wasm-eval-h-i64binop-compare (i :i64binop x :i64 y :i64) :wasm-ops-h-out
+  (match i
+    ('eq64 (if (= x y) 1 0))
+    ('ne64 (if (= x y) 0 1))
+    ('lt_s64 (if (< (sign-interpretation-64 x) (sign-interpretation-64 y)) 1 0))
+    ('lt_u64 (if (< x y) 1 0))
+    ('gt_u64 (if (> x y) 1 0))
+    ('gt_s64 (if (> (sign-interpretation-64 x) (sign-interpretation-64 y)) 1 0))
+    ('le_u64 (if (<= x y) 1 0))
+    ('le_s64 (if (<= (sign-interpretation-64 x) (sign-interpretation-64 y)) 1 0))
+    ('ge_u64 (if (>= x y) 1 0))
+    ('ge_s64 (if (>= (sign-interpretation-64 x) (sign-interpretation-64 y)) 1 0))
+    (& "this error shouldn't happen. in: wasm-eval-h-i32binop-compare")))
+
 
 (definec wasm-eval-h-i64binop (i :i64binop s :stack) :wasm-eval-h-out
-  (let ((x (car s))
-        (y (cadr s)))
-    (if (or (not x) (not y))
-      "not enough variables declared to do i64binop!"
-      (if (and (i64p x) (i64p y))
-      (match i
-	('add64 (cons (rem (+ x y) (expt 2 64)) (cddr s)))
-	('sub64 (cons (rem (+ (- x y) (expt 2 64)) (expt 2 64)) (cddr s)))
-	('mul64 (cons (rem (* x y) (expt 2 64))(cddr s)))
-	('div_u64 (if (zerop y) "div by 0 error" (cons (truncate x y) (cddr s))))
-	('rem_u64 (if (zerop y) "rem by 0 error" (cons (rem x y) (cddr s))))
-        ) "not i64 inputs!"))))
+  :skip-function-contractp t
+  (match s
+    (nil "not enough arguments!")
+    ((x . k)
+     (match k
+       (nil "not enough arguments!")
+       ((y . r) (if (and (i64p x) (i64p y))
+                    (match i
+	            ((:or 'add64 'sub64 'mul64 'div_u64 'rem_u64 'div_s64 'rem_s64)
+	             (let ((res (wasm-eval-h-i64binop-arith i x y)))
+		       (if (not (constp x)) "result was not a const"
+			 (if (wasm-eval-h-outp res) res (cons res r)))))  
+	            ((:or 'and64 'or64 'xor64)
+	             (let ((res (wasm-eval-h-i64binop-bitwise i x y)))
+		       (if (not (constp x)) "result was not a const"
+			 (if (wasm-eval-h-outp res) res (cons res r)))))  
+                   (& (let ((res (wasm-eval-h-i64binop-compare i x y)))
+			(if (not (constp x)) "result was not a const"
+			  (if (wasm-eval-h-outp res) res (cons res r)))))) 
+	         "not i64 inputs!"))
+    ))))
+
 
 (property (s :stack)
   (implies (not (equal s nil)) (val-or-errorp (car s))))
@@ -292,7 +369,9 @@ which is a sum type of either an exception String or a program stack.
 (property (s :string)
   (val-or-errorp s))
 
-(definec wasm-eval-h (p :wasm-program s :stack) :val-or-error
+(definec wasm-eval-h (p :wasm-function s :stack) :val-or-error
+  :skip-function-contractp t
+  :skip-body-contractsp t
   (match p
     (nil (match s
 	   (nil "No value to be returned!")
@@ -311,8 +390,7 @@ which is a sum type of either an exception String or a program stack.
 	       (:i64binop
 		(let ((x (wasm-eval-h-i64binop f s)))
 		  (if (Stringp x) x (wasm-eval-h r x))))
-	       (:nop (wasm-eval-h r s)))))
-  :timeout 300)
+	       (:nop (wasm-eval-h r s))))))
 
-(definec wasm-eval (p :wasm-program) :val-or-error
+(definec wasm-eval (p :wasm-function) :val-or-error
   (wasm-eval-h p nil))
